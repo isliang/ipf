3f6142dc66e4efea552f7c4c5cccb8288b76970e
王世亮 <wslhdu@163.com>
2020-10-20 14:13:01 +0800

    add request

---
+++ src/Http/Request.php
@@ -0,0 +1,75 @@
+<?php
+namespace Ipf\Http;
+
+use Ipf\Utils\TSingleton;
+
+class Request
+{
+    use TSingleton;
+    /**
+     * @var \GuzzleHttp\Psr7\Request
+     */
+    private $request;
+
+    /**
+     * @var array
+     */
+    private $query = [];
+
+    /**
+     * @var array
+     */
+    private $post = [];
+
+    private $is_init = false;
+
+    public function __construct()
+    {
+        $headers = [];
+        foreach ($_SERVER as $key => $value) {
+            if (strpos($key, 'HTTP_') === 0) {
+                $headers[$key] = $value;
+            }
+        }
+        $this->request = new \GuzzleHttp\Psr7\Request(
+            $_SERVER['REQUEST_METHOD'],
+            $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'],
+            $headers,
+            file_get_contents('php://input')
+        );
+    }
+
+    /**
+     * @param null $name
+     * @return array|mixed
+     * 返回get参数
+     */
+    public function getQuery($name = null)
+    {
+        if (empty($this->query) && $query = $this->request->getUri()->getQuery()) {
+            parse_str($query, $this->query);
+        }
+        return is_null($name) ? $this->query : $this->query[$name];
+    }
+
+    public function getPost($name = null)
+    {
+        if (empty($this->post) && $post = $this->request->getBody()) {
+            switch ($this->getHeader('HTTP_CONTENT_TYPE')) {
+                case 'application/x-www-form-urlencoded':
+                    parse_str($post, $this->post);
+                    break;
+                case 'application/json':
+                    $this->post = json_decode($post, true);
+                    break;
+            }
+        }
+        return is_null($name) ? $this->post : $this->post[$name];
+    }
+
+    public function getHeader($name = null)
+    {
+        return is_null($name) ? $this->request->getHeaders() :
+            $this->request->getHeader($name);
+    }
+}
---
+++ src/Utils/TSingleton.php
@@ -0,0 +1,26 @@
+<?php
+namespace Ipf\Utils;
+
+trait TSingleton
+{
+    protected function __construct()
+    {
+
+    }
+    protected function __clone()
+    {
+
+    }
+    public static function getInstance()
+    {
+        static $instance = null;
+        if (!$instance) {
+            $ref = new \ReflectionClass(get_called_class());
+            $ctor = $ref->getConstructor();
+            $self = $ref->newInstanceWithoutConstructor();
+            $ctor->setAccessible(true);
+            $instance = $ctor->invokeArgs($self, func_get_args());
+        }
+        return $instance;
+    }
+}
