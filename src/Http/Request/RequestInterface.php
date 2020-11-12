0244aa6d9654ffa4e51cfa383f4bb953cba9c45a
王世亮 <wslhdu@163.com>
2020-11-12 16:19:42 +0800

    add

--- src/Http/Request/RequestInterface.php
+++ src/Http/Request/RequestInterface.php
@@ -3,10 +3,34 @@ namespace Ipf\Http\Request;
 
 interface RequestInterface
 {
+    /**
+     * @param string|null $name
+     * @return mixed
+     */
     public function getQuery($name = null);
+
+    /**
+     * @param string|null $name
+     * @return mixed
+     */
     public function getPost($name = null);
+
+    /**
+     * @param string|null $name
+     * @return mixed
+     */
     public function getFile($name = null);
+
+    /**
+     * @param string|null $name
+     * @return mixed
+     */
     public function getHeader($name = null);
+
+    /**
+     * @param string|null $name
+     * @return mixed
+     */
     public function getCookie($name = null);
     public function getIp();
     public function getMethod();
--- src/Http/Response/ResponseInterface.php
+++ src/Http/Response/ResponseInterface.php
@@ -3,9 +3,41 @@ namespace Ipf\Http\Response;
 
 interface ResponseInterface
 {
-    public function status(int $status);
-    public function header(string $key, string $value);
+    /**
+     * @param int $status
+     * @return mixed
+     */
+    public function status($status);
+
+    /**
+     * @param string $key
+     * @param string $value
+     * @return mixed
+     */
+    public function header($key, $value);
+
+    /**
+     * @param $name
+     * @param string $value
+     * @param int $expire
+     * @param string $path
+     * @param string $domain
+     * @param bool $secure
+     * @param bool $httponly
+     * @return mixed
+     */
     public function cookie($name, $value = "", $expire = 0, $path = "", $domain = "", $secure = false, $httponly = false);
-    public function redirect(string $url, int $code);
-    public function send(string $data);
+
+    /**
+     * @param string $url
+     * @param int $code
+     * @return mixed
+     */
+    public function redirect($url, $code);
+
+    /**
+     * @param string $data
+     * @return mixed
+     */
+    public function send($data);
 }
